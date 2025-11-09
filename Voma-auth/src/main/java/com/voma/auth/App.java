21642526a988b729eaf6b9e18805b55fcd17d326
Your Name <vomaone@gmail.com>
2025-11-09 15:20:56 -0800

    .

---
+++ Voma-auth/src/main/java/com/voma/auth/App.java
@@ -0,0 +1,13 @@
+package com.voma.auth;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
